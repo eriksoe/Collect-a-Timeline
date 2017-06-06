c36ee54e0f08eaf693092397607b5e4e70c0af9f
Erik Søe Sørensen <ess@trifork.com>
2017-06-06 21:25:09 +0200

    Drawing in 3D...

--- timeline.py
+++ timeline.py
@@ -98,6 +98,8 @@ def page2():
     ph = 20.0 * cm #scale*PHEIGHT
 
     # Paper sheet grid:
+    gsave()
+    setlinewidth(0.02*MM)
     for i in range(0,4+1):
         newpath()
         moveto(i*pw, 0)
@@ -105,6 +107,7 @@ def page2():
         moveto(0,    i*ph)
         lineto(4*pw, i*ph)
         stroke()
+    grestore()
 
     # Bottom part of timeline:
     moveto(PART1_XMAX*pw, PART1_Y*ph)
@@ -124,43 +127,74 @@ def page2():
         draw_tick((3+0.75)*pw - 100*i*YEAR*scale, 0.5*ph, YEAR_TICK, 0)
 
     # Far wall outline:
-    moveto(5*cm,40*cm)
-    rlineto(80*cm,0)
-    rlineto(0,40*cm)
-    rlineto(-80*cm,0)
-    rlineto(0,-40*cm)
+    depth = 40*100*cm
+    far_wall_width = 50*80*cm
+    far_wall_height = 50*40*cm
+    floor_z = (40-100)*cm
+    rect3d((95*cm,floor_z, depth), (0,far_wall_height, 0), (-far_wall_width,0, 0))
     stroke()
+    # moveto(5*cm,40*cm)
+    # rlineto(80*cm,0)
+    # rlineto(0,40*cm)
+    # rlineto(-80*cm,0)
+    # rlineto(0,-40*cm)
+    # stroke()
 
     # Into-depth wall:
-    moveto(85*cm,40*cm)
-    rlineto(10*cm,0)
-    rlineto(0,20*cm)
-    rlineto(-10*cm,0)
-    rlineto(0,-20*cm)
+    near_wall_depth = 10*cm
+    # - Corner:
+    moveto3d(95*cm,40*cm, 0)
+    lineto3d(95*cm,40*cm, near_wall_depth)
+    stroke()
+    # rect3d((95*cm,40*cm, 0), (0,20*cm, 0), (0,0, depth))
+    rect3d((95*cm,floor_z, 0), (0,300*cm, 0), (0,0, depth))
+    # moveto(85*cm,40*cm)
+    # rlineto(10*cm,0)
+    # rlineto(0,20*cm)
+    # rlineto(-10*cm,0)
+    # rlineto(0,-20*cm)
+    # stroke()
+
+    # Near wall top:
+    rect3d((95*cm,40*cm, 0), (-100*cm, 0, 0), (0,0, near_wall_depth))
+
+
+    rect3d((95*cm,40*cm, 0), (0,20*cm, 0), (0,0, depth))
+    rect3d((95*cm,40*cm, 0), (0,100*cm, 0), (0,0, depth))
+
+    rect3d((95*cm,40*cm, depth), (0,20*cm, 0), (-far_wall_width,0, 0))
+    # moveto3d(95*cm,40*cm, 0)
+    # lineto3d(95*cm,40*cm, 10*100*cm)
+    # moveto3d(95*cm,60*cm, 0)
+    # lineto3d(95*cm,60*cm, 10*100*cm)
+    # rlineto(10*cm,0)
+    # rlineto(0,20*cm)
+    # rlineto(-10*cm,0)
+    # rlineto(0,-20*cm)
     stroke()
 
     # Vanishing point and lines thereto:
-    moveto(84.75*cm, 45*cm)
-    lineto(95*cm,40*cm)
+    # moveto(84.75*cm, 45*cm)
+    # lineto(95*cm,40*cm)
 
-    moveto(84.75*cm, 45*cm)
-    lineto(95*cm,60*cm)
+    # moveto(84.75*cm, 45*cm)
+    # lineto(95*cm,60*cm)
 
-    moveto(84.75*cm, 45*cm)
-    lineto(95*cm,80*cm)
+    # moveto(84.75*cm, 45*cm)
+    # lineto(95*cm,80*cm)
 
-    moveto(84.75*cm, 45*cm)
-    lineto(95*cm,0*cm)
+    # moveto(84.75*cm, 45*cm)
+    # lineto(95*cm,0*cm)
 
-    moveto(84.75*cm, 45*cm)
-    lineto(0*cm,0*cm)
+    # moveto(84.75*cm, 45*cm)
+    # lineto(0*cm,0*cm)
 
-    moveto(84.75*cm, 45*cm)
-    lineto(0*cm,40*cm)
+    # moveto(84.75*cm, 45*cm)
+    # lineto(0*cm,40*cm)
 
-    moveto(84.75*cm, 45*cm)
-    lineto(0*cm,45*cm)
-    stroke()
+    # moveto(84.75*cm, 45*cm)
+    # lineto(0*cm,45*cm)
+    # stroke()
 
     end_page()
 
@@ -184,6 +218,11 @@ def rlineto(x,y): coord(x,y); print "rlineto "
 def arct(x1,y1,x2,y2, r): coord(x1,y1); coord(x2,y2); size(r); print "arct "
 def stroke(): print "stroke\n"
 
+def gsave(): print "save\n"
+def grestore(): print "restore\n"
+
+
+
 def textover(s): print "(%s) center show" % s
 def draw_tick(x,y, ticksize, dir):
     rad_dir = dir / 180.0 * pi
@@ -193,6 +232,42 @@ def draw_tick(x,y, ticksize, dir):
     rlineto(2*xsz, 2*ysz)
     # moveto(nowx-i*YEAR, nowy-ticksize)
     # lineto(nowx-i*YEAR, nowy+ticksize)
+
+
+#========== 3D graphics: ========================================
+SCALE = 1/4.0
+CM = SCALE/2.54*72
+def vanishing_point():
+    return (84.75*CM, 45*CM)
+def near_plane_depth(): return 80*CM
+def convert3d(x,y,z):
+    (x0,y0) = (x,y)
+    # Depth=0 is the near plane.
+    (vx,vy) = vanishing_point();
+    npz = near_plane_depth()
+    x -= vx; y -= vy
+    d = 1 + z/npz
+    x /= d; y /= d
+    x += vx; y += vy
+    print "%% %s -> %s" % ((x0,y0,z), (x,y))
+    return (x,y)
+def moveto3d(x,y,z):
+    (x,y) = convert3d(x,y,z)
+    moveto(x,y)
+def lineto3d(x,y,z):
+    (x,y) = convert3d(x,y,z)
+    lineto(x,y)
+
+def rect3d(p, v1,v2):
+    (x,y,z) = p
+    (dx1,dy1,dz1) = v1
+    (dx2,dy2,dz2) = v2
+    moveto3d(x,y,z)
+    lineto3d(x+dx1, y+dy1, z+dz1)
+    lineto3d(x+dx1+dx2, y+dy1+dy2, z+dz1+dz2)
+    lineto3d(x+dx2, y+dy2, z+dz2)
+    lineto3d(x,y,z)
+#================================================================
 
 
 def size(x): print "%.3f " % (x)
